fcee9d52bd6b16145072b5253d891548ba859a3c
zhaoguo Wang <2515112W@student.gla.ac.uk>
2021-03-15 22:19:05 +0000

    6 commit

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -18,6 +20,9 @@ def index(request):
     context_dict['categories'] = category_list
     
     return render(request, 'rango/index.html', context = context_dict)
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>" )
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -35,5 +40,21 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
     
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            #save the new category to the database
+            form.save(commit=True)
+            #Now that the category is saved, we could confirm this.
+            #For now, just redirect the user back to the index view
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form':form})
+            
         
         
